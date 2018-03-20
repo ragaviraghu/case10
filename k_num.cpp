5c90bb19f0ba44e1c0ac2600adeac009d4cdaed0
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-20 10:11:19 -0700

    Update k_num.cpp

--- k_num.cpp
+++ k_num.cpp
@@ -7,10 +7,10 @@ cout<<"Enter the N value : "<<endl;
 cin>>N;
 cout<<"Enter the K value : "<<endl;
 cin>>K;
-for(int i=0;i<N;i++)
+for(int i=1;i<=N;i++)
 {
 cin>>arr[i];
 }
-cout<<"The  element in K th position is : "<<arr[K-1]<<endl;
+cout<<"The  element in K th position is : "<<arr[K]<<endl;
 return 0;
 }

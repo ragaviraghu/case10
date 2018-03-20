4569c4bcd6ea5d85c972f905bf9d9be9addd9183
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-20 10:28:28 -0700

    Create abc_mod.cpp

---
+++ abc_mod.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+int a,b,c,result;
+cout<<"Enter the a value : "<<endl;
+cin>>a;
+cout<<"Enter the b value : "<<endl;
+cin>>b;
+cout<<"Enter the c value : "<<endl;
+cin>>c;
+result=(a*b)%c;
+cout<<"The result is : "<<result<<endl;
+return 0;
+}

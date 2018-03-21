c13346f53675fff247efe883e5f60acceac4332f
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-21 10:34:11 +0530

    Create SI.cpp

---
+++ SI.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() 
+{
+int P,T,R,SI;
+cout<<"Enter the value of P,T and R : "<<endl;
+cin>>P>>T>>R;
+SI=(P*T*R)/100;
+cout<<" Simple Interest is : "<<SI<<endl;
+return 0;
+}

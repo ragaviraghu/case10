fc8ea90de679aaed2dfee981aa33a4f53be7b729
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-21 10:17:07 +0530

    Create Kelvin.cpp

---
+++ Kelvin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+float celsius,kelvin;
+cout<<"Enter the temperature in celsius : "<<endl;
+cin>>celsius;
+kelvin=celsius+273;
+cout<<" Temperature in kelvin is : "<<kelvin<<endl;
+return 0;
+}
